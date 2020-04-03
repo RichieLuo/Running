b81c6682c63b9601d16f814a885fdd7d5e15d79d
RichieLuo <richieluo@msn.com>
2020-04-04 06:02:40 +0800

    修改文件夹
    修改文件夹

--- Assets/Script/Menu/BackgroundScroll.js
+++
@@ -1,9 +0,0 @@
-﻿#pragma strict
-
-function Start () {
-
-}
-
-function Update () {
-	GetComponent.<Renderer>().material.SetTextureOffset("_MainTex", Vector2(-Time.time * .1, Mathf.Cos(Time.time * .1)));
-}
